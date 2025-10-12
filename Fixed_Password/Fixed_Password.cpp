5c8d5a0672c6ee32ae66f454d033e0539c179605
KajalBormon <kajalbormon40@gmail.com>
2025-10-13 01:01:43 +0600

    Fixed password

---
+++ Fixed_Password/Fixed_Password.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std; 
+
+int main() {
+    int n; 
+    while(cin >> n) {
+        if(n == 1999) {
+            cout << "Correct" << endl;
+            break;
+        } else {
+            cout << "Wrong" << endl;
+        }
+    }
+    return 0; 
+}
