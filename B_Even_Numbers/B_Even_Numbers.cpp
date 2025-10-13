5fd7eb185607b4d2c078085114eb6d270190015d
KajalBormon <kajalbormon40@gmail.com>
2025-10-13 13:46:49 +0600

    even numbers

--- B_Even_Numbers/B_Even_Numbers.cpp
+++ B_Even_Numbers/B_Even_Numbers.cpp
@@ -7,10 +7,15 @@ int main() {
     int n; 
     cin >> n; 
 
+    if(n == 1) {
+        cout << "-1";
+    }
+
     for(int i = 1; i <= n; i++) {
         if(i % 2 == 0) {
             cout << i << endl;
         }
     }
+
     return 0; 
 }
