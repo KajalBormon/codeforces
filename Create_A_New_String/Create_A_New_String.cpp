5b758ce2885f80df2796510b03ed127e04a99e5a
KajalBormon <kajalbormon40@gmail.com>
2025-10-20 21:53:51 +0600

    Create a string

---
+++ Create_A_New_String/Create_A_New_String.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std; 
+
+int main() {
+    string s, t; 
+    cin >> s;
+    cin >> t;
+
+    cout << s.size() << " " << t.size() << endl;
+    cout << s << " " << t << endl; 
+    return 0; 
+}
