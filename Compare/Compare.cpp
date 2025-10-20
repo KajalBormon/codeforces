2459341d3b048499eef4338d8a0278a6b069e940
KajalBormon <kajalbormon40@gmail.com>
2025-10-20 23:31:17 +0600

    Compare

---
+++ Compare/Compare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    string a, b; 
+    cin >> a; 
+    cin >> b; 
+    
+    if(a < b)
+        cout << a;
+    else 
+        cout << b; 
+    return 0; 
+}
