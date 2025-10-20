96dbf1e7f5bf53cf7acf0882ffd6906ca9527191
KajalBormon <kajalbormon40@gmail.com>
2025-10-20 23:38:03 +0600

    Count

---
+++ Count/Count.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+using namespace std; 
+
+int main() {
+    string s; 
+    cin >> s; 
+    
+    int sum = 0;
+    for(int i = 0; i < int(s.size()); i++) {
+        sum += s[i] - '0';
+    }
+
+    cout << sum;
+    return 0; 
+}
